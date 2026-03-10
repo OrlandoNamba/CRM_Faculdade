5ff143d71c3db3e3712f153c693f1a9a121b3839
OrlandoNamba <dev.onamba@gmail.com>
2026-03-10 13:33:17 -0300

    feat: adiciona script para inserir clientes no banco de dados

---
+++ inserir_cliete.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+conexao = sqlite3.connect('database/crm.db') #Conecta ao banco de dados "crm.db", e armazena a conexão na variável "conexao"
+cursor = conexao.cursor() #Cria um cursor para executar comandos SQL, e armazena o cursor na variável "cursor"
+
+cursor.execute("""
+INSERT INTO clientes (nome, cpf, cidade, telefone) 
+VALUES (?, ?, ?, ?)
+""", ("João Silva", "12345678901", "São Paulo", "1199999999")) #Executa um comando SQL para inserir um novo cliente na tabela "clientes" com os valores especificos
+
+conexao.commit() #Salva as alterações no banco de dados
+conexao.close() #Fecha a conexão com o banco de dados
