fcb221e7b024a5e7e72ef09072f2b7f83f4ecf8c
OrlandoNamba <dev.onamba@gmail.com>
2026-03-11 21:24:43 -0300

    feat: transforma o listar_clientes em função

--- listar_clientes.py
+++ listar_clientes.py
@@ -1,12 +1,16 @@
 from conexao import conectar_banco
 
-conexao = conectar_banco() # Chama a função "conectar_banco" para obter a conexão com o banco de dados e armazena a conexão na variável "conexao"
-cursor = conexao.cursor() # Cria um cursor para executar comandos SQL, e armazena o cursor na variável "cursor" 
+def listar_clientes():
+    conexao = conectar_banco() # Chama a função "conectar_banco" para obter a conexão com o banco de dados e armazena a conexão na variável "conexao"
+    cursor = conexao.cursor() # Cria um cursor para executar comandos SQL, e armazena o cursor na variável "cursor" 
 
-cursor.execute("SELECT * FROM clientes") #Executa um comando SQL para selecionar todos os registros da tabela "clientes"
-clientes = cursor.fetchall() #Armazena os resultados da consulta na variável "clientes"
+    cursor.execute("SELECT * FROM clientes") #Executa um comando SQL para selecionar todos os registros da tabela "clientes"
+    clientes = cursor.fetchall() #Armazena os resultados da consulta na variável "clientes"
 
-for cliente in clientes:
-    print(cliente) #Cada cliente vai ser mostrado como tupla
+    for cliente in clientes:
+        print(cliente) #Cada cliente vai ser mostrado como tupla
 
-conexao.close() #Fecha a conexão com o banco de dados
+    conexao.close() #Fecha a conexão com o banco de dados
+
+if __name__ == "__main__":
+    listar_clientes()
