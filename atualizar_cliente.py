5d3d5f610d024a206db0c97d5bf652a342da49a9
OrlandoNamba <dev.onamba@gmail.com>
2026-03-13 07:21:02 -0300

    feat: permite atualizar cliente mantendo valores antigos com ENTER

--- atualizar_cliente.py
+++ atualizar_cliente.py
@@ -5,7 +5,7 @@ def atualizar_cliente():
     conexao = conectar_banco() # Chama a função "conectar_banco" para obter a conexão com o banco de dados e armazena a conexão na variável "conexao"
     cursor = conexao.cursor() # Cria um cursor para executar comandos SQL, e armazena o cursor na variável "cursor"
 
-    cpf = input("Digite o CPF do cliente que deseja atualizar: ") # Solicita ao usuário que digite o CPF do cliente que deseja atualizar e armazena a entrada na variável "cpf"
+    cpf = input("Digite o CPF do cliente que deseja atualizar: ")
 
     cursor.execute("SELECT * FROM clientes WHERE cpf = ?",
                 (cpf,))
@@ -15,15 +15,18 @@ def atualizar_cliente():
         print("Cliente encontrado:")
         print("Nome:", cliente["nome"])
         print("CPF:", cliente["cpf"])
+        print("Cidade:", cliente["cidade"])
         print("Telefone:", cliente["telefone"])
-        novo_telefone = input("Digite o novo telefone do cliente: ") # Solicita ao usuário que digite o novo telefone do cliente e armazena a entrada na variável "novo_telefone"
-        cursor.execute("UPDATE clientes SET telefone = ? WHERE cpf = ?",
-                    (novo_telefone, cpf)) # Executa um comando SQL para atualizar o telefone do cliente com o CPF especificado, usando parâmetros para evitar SQL injection
+        novo_nome = input("Novo Nome (ENTER para manter o atual): ")
+        nova_cidade = input("Nova Cidade (ENTER para manter a atual): ")
+        novo_telefone = input("Novo Telefone (ENTER para manter o atual): ")
+        cursor.execute("UPDATE clientes SET nome = ?, cidade = ?, telefone = ? WHERE cpf = ?",
+                    (novo_nome, nova_cidade, novo_telefone, cpf)) # Executa um comando SQL para atualizar o telefone do cliente com o CPF especificado, usando parâmetros para evitar SQL injection
         
         conexao.commit() # Salva as alterações no banco de dados
-        print("Telefone atualizado com sucesso.") # Imprime uma mensagem indicando que o telefone foi atualizado
+        print("Cliente atualizado com sucesso.")
     else:
-        print("Cliente não encontrado") # Se o cliente não for encontrado, imprime uma mensagem indicando que o cliente não foi encontrado
+        print("Cliente não encontrado")
 
     conexao.close() # Fecha a conexão com o banco de dados
 
