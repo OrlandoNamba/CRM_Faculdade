12c61fe2a3da845dcf46c080a264c12014d1be53
OrlandoNamba <dev.onamba@gmail.com>
2026-03-18 06:59:45 -0300

    feat: funções no padrão service/repository

--- clientes/services.py
+++ clientes/services.py
@@ -1,29 +1,45 @@
 import sqlite3
 from database.conexao import conectar_banco
 from validacoes.validacoes import validar_cpf, validar_telefone
+from clientes.repository import deletar_cliente_por_cpf, inserir_cliente
+from clientes.repository import buscar_cliente_por_cpf
+from clientes.repository import listar_todos_clientes
+from clientes.repository import atualizar_cliente
 
 def cadastrar_cliente(nome, cpf, cidade, telefone):
-    conexao = conectar_banco()
-    cursor = conexao.cursor()
 
     if not validar_cpf(cpf):
-        conexao.close()
         return "cpf_invalido"
 
     if not validar_telefone(telefone):
-        conexao.close()
         return "telefone_invalido"
+    
+    resultado = inserir_cliente(nome, cpf, cidade, telefone)
 
-    try:
-        cursor.execute("""
-        INSERT INTO clientes (nome, cpf, cidade, telefone) 
-        VALUES (?, ?, ?, ?)
-        """, (nome, cpf, cidade, telefone))
+    return resultado
 
-        conexao.commit()
-        conexao.close()
-        return "sucesso"
+def obter_cliente_por_cpf(cpf):
+    cliente = buscar_cliente_por_cpf(cpf)
+    return cliente
 
-    except sqlite3.IntegrityError:
-        conexao.close()
-        return "cpf_existente"
+def obter_todos_clientes():
+    clientes = listar_todos_clientes()
+    return clientes
+
+def atualizar_cliente_service(id_cliente, nome, cpf, cidade, telefone):
+    if not validar_cpf(cpf):
+        return "cpf_invalido"
+
+    if not validar_telefone(telefone):
+        return "telefone_invalido"
+    
+    resultado = atualizar_cliente(id_cliente, nome, cpf, cidade, telefone)
+
+    return resultado
+
+def deletar_cliente_service(cpf):
+    if not validar_cpf(cpf):
+        return "cpf_invalido"
+
+    resultado = deletar_cliente_por_cpf(cpf)
+    return resultado
