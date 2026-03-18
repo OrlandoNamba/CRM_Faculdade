f9b43114be9d745bad1818d4f7342dfb2e1261ce
OrlandoNamba <dev.onamba@gmail.com>
2026-03-18 12:42:49 -0300

    refactor: exclui pasta duplicada

--- clientes/cliente_utils.py
+++
@@ -1,7 +0,0 @@
-def mostrar_cliente(cliente):
-    print("ID:", cliente["id_cliente"])
-    print("Nome:", cliente["nome"])
-    print("CPF:", cliente["cpf"])
-    print("Cidade:", cliente["cidade"])
-    print("Telefone:", cliente["telefone"])
-    print("-" * 20)
