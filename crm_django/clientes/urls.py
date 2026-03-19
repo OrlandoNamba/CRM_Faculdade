7d1b770ccc06d0c075c1a3a12e415c7f45bc5884
OrlandoNamba <dev.onamba@gmail.com>
2026-03-19 07:05:57 -0300

    feat: implementa cadastro de clientes via formulário Django na web

--- crm_django/clientes/urls.py
+++ crm_django/clientes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import listar_clientes_view
+from .views import cadastrar_cliente_view, listar_clientes_view
 
 urlpatterns = [
     path('clientes/', listar_clientes_view),
+    path('clientes/cadastrar/', cadastrar_cliente_view)
 ]
--- crm_django/clientes/views.py
+++ crm_django/clientes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core_clientes.services import obter_todos_clientes
+from core_clientes.services import cadastrar_cliente
 
 def listar_clientes_view(request):
     clientes = obter_todos_clientes()
@@ -7,3 +8,21 @@ def listar_clientes_view(request):
     return render(request, 'clientes/listar_clientes.html', 
                   {'clientes': clientes
     })
+
+def cadastrar_cliente_view(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        cpf = request.POST.get('cpf')
+        cidade = request.POST.get('cidade')
+        telefone = request.POST.get('telefone')
+
+        resultado = cadastrar_cliente(nome, cpf, cidade, telefone)
+
+        if resultado == "sucesso":
+            return redirect('/clientes/')
+        
+        return render(request, 'clientes/cadastrar_clientes.html', {
+            'erro': resultado
+        })
+
+    return render(request, 'clientes/cadastrar_clientes.html')
