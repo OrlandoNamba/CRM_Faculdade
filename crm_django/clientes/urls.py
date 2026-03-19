8a4d663698da55cc3f3edd1ef4f4611029c53aba
OrlandoNamba <dev.onamba@gmail.com>
2026-03-19 07:43:48 -0300

    feat: implementa edição de clientes via Django na web

--- crm_django/clientes/urls.py
+++ crm_django/clientes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import cadastrar_cliente_view, listar_clientes_view
+from .views import cadastrar_cliente_view, editar_cliente_view, listar_clientes_view
 
 urlpatterns = [
     path('clientes/', listar_clientes_view),
-    path('clientes/cadastrar/', cadastrar_cliente_view)
+    path('clientes/cadastrar/', cadastrar_cliente_view),
+    path('clientes/editar/<str:cpf>/', editar_cliente_view)
 ]
--- crm_django/clientes/views.py
+++ crm_django/clientes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from core_clientes.services import obter_todos_clientes
 from core_clientes.services import cadastrar_cliente
+from core_clientes.services import obter_cliente_por_cpf, atualizar_cliente_service
 
 def listar_clientes_view(request):
     clientes = obter_todos_clientes()
@@ -26,3 +27,33 @@ def cadastrar_cliente_view(request):
         })
 
     return render(request, 'clientes/cadastrar_clientes.html')
+
+def editar_cliente_view(request, cpf):
+    cliente = obter_cliente_por_cpf(cpf)
+
+    if not cliente:
+        return redirect('/clientes/')
+    
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        cidade = request.POST.get('cidade')
+        telefone = request.POST.get('telefone')
+
+        resultado = atualizar_cliente_service(
+            cliente['id_cliente'],
+            nome,
+            cliente['cpf'],
+            cidade,
+            telefone
+        )
+
+        if resultado == "sucesso":
+            return redirect('/clientes/')
+        
+        return render(request, 'clientes/editar_clientes.html', {
+            'cliente': cliente,
+            'erro': resultado
+        })
+    return render(request, 'clientes/editar_clientes.html', {
+        'cliente': cliente
+    })
