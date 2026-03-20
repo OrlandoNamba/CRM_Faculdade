c81527e212ed1730bd3a7414895fd2a5219baa61
OrlandoNamba <dev.onamba@gmail.com>
2026-03-20 12:52:56 -0300

    feat: esconde botão Todos os Clientes, quando já estiver na página de listagem

--- crm_django/clientes/urls.py
+++ crm_django/clientes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import cadastrar_cliente_view, editar_cliente_view, listar_clientes_view, deletar_cliente_view
 
 urlpatterns = [
-    path('clientes/', listar_clientes_view),
-    path('clientes/cadastrar/', cadastrar_cliente_view),
-    path('clientes/editar/<str:cpf>/', editar_cliente_view),
-    path('clientes/deletar/<str:cpf>/', deletar_cliente_view)
+    path('clientes/', listar_clientes_view, name='listar_clientes'),
+    path('clientes/cadastrar/', cadastrar_cliente_view, name='cadastrar_cliente'),
+    path('clientes/editar/<str:cpf>/', editar_cliente_view, name='editar_cliente'),
+    path('clientes/deletar/<str:cpf>/', deletar_cliente_view, name='deletar_cliente')
 ]
