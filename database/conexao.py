34813dd630f660d5a17b7f153be9c7882a1a70c5
OrlandoNamba <dev.onamba@gmail.com>
2026-03-23 07:29:36 -0300

    feat: adiciona novos campos telefone2, telefone3 e renda na tabela clientes

--- database/conexao.py
+++ database/conexao.py
@@ -6,3 +6,25 @@ def conectar_banco():
     conexao = sqlite3.connect('database/crm.db') # Conecta ao banco de dados "crm.db", e armazena a conexão na variável "conexao"
     conexao.row_factory = sqlite3.Row # Configura a conexão para retornar os resultados das consultas como objetos do tipo "Row", permitindo acessar os campos por nome
     return conexao # Retorna a conexão com o banco de dados
+
+def atualizar_tabela_clientes():
+    conexao = conectar_banco() # Estabelece a conexão com o banco de dados
+    cursor = conexao.cursor() # Cria um cursor para executar comandos SQL
+
+    try:
+        cursor.execute("ALTER TABLE clientes ADD COLUMN telefone2 VARCHAR(15)")
+    except:
+        pass
+    
+    try:
+        cursor.execute("ALTER TABLE clientes ADD COLUMN telefone3 VARCHAR(15)")
+    except:
+        pass
+    
+    try:
+        cursor.execute("ALTER TABLE clientes ADD COLUMN renda DECIMAL(10,2)")
+    except:
+        pass
+
+    conexao.commit() # Salva as alterações no banco de dados
+    conexao.close() # Fecha a conexão com o banco de dados
