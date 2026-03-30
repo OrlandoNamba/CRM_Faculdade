67b039fbf3bc355e7b6a0aaa3c588940f67b39b3
OrlandoNamba <dev.onamba@gmail.com>
2026-03-30 20:14:15 -0300

    refactor: transfere a criação do banco de dados para o script start_crm

--- crm_django/start_crm.py
+++ crm_django/start_crm.py
@@ -2,12 +2,17 @@ import os
 import webbrowser
 import threading
 from django.core.management import execute_from_command_line
+from database.criar_banco import criar_tabelas
+
 
 def abrir_navegador():
     webbrowser.open('http://127.0.0.1:8000/')
 
+
 if __name__ == "__main__":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_django.settings')
+
+    criar_tabelas()
 
     threading.Timer(1.5, abrir_navegador).start()
 
