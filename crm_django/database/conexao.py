fb697a1e2f69e6eee81dd9f7dc542ae1d11816b9
OrlandoNamba <dev.onamba@gmail.com>
2026-03-30 20:52:42 -0300

    refactor: corrige erro ao executar o programa por arquivo .exe

--- crm_django/database/conexao.py
+++ crm_django/database/conexao.py
@@ -1,8 +1,19 @@
 import sqlite3
 import os
+import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def get_base_dir():
+    # Se estiver rodando como .exe
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    # Se estiver rodando pelo VSCode / Python normal
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+BASE_DIR = get_base_dir()
 CAMINHO_BANCO = os.path.join(BASE_DIR, 'database', 'crm.db')
+
 
 def conectar_banco():
     print("BANCO EM USO:", CAMINHO_BANCO)
